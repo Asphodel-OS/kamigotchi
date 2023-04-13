8224292ca1ec56efa84a1511fbffc38c6c248932
ashys <40616911+chlwys@users.noreply.github.com>
2023-04-13 14:50:18 +0900

    mint/burn -> withdraw/deposit

--- packages/contracts/src/systems/ERC20WithdrawSystem.sol
+++ packages/contracts/src/systems/ERC20WithdrawSystem.sol
@@ -9,10 +9,11 @@ import { LibCoin } from "libraries/LibCoin.sol";
 
 import { KamiERC20 } from "tokens/KamiERC20.sol";
 
-uint256 constant ID = uint256(keccak256("system.ERC20.Burn"));
+uint256 constant ID = uint256(keccak256("system.ERC20.Withdraw"));
 
-// brings ERC20 tokens back into the game, sends it to the sender's account entity
-contract ERC20BurnSystem is System {
+// brings in game coins into the real world by minting ERC20 tokens in the ERC20 contract
+// sends it only to the Account owner's address
+contract ERC20WithdrawSystem is System {
   address token;
 
   constructor(IWorld _world, address _components) System(_world, _components) {}
@@ -23,22 +24,20 @@ contract ERC20BurnSystem is System {
   }
 
   function execute(bytes memory arguments) public returns (bytes memory) {
-    require(token != address(0), "ERC20BurnSystem: not inited");
+    require(token != address(0), "ERC20WithdrawSystem: not inited");
 
-    uint256 amount = abi.decode(arguments, (uint256));
+    (address to, uint256 amount) = abi.decode(arguments, (address, uint256));
     uint256 accountID = LibAccount.getByOwner(components, msg.sender);
 
-    if (accountID == 0) {
-      accountID = LibAccount.create(world, components, msg.sender, msg.sender);
-    }
+    require(accountID != 0, "ERC20WithdrawSystem: to address has no account");
 
-    KamiERC20(token).burn(msg.sender, amount);
-    LibCoin.inc(components, accountID, amount);
+    LibCoin.dec(components, accountID, amount);
+    KamiERC20(token).withdraw(to, amount);
 
     return "";
   }
 
-  function executeTyped(uint256 amount) public returns (bytes memory) {
-    return execute(abi.encode(amount));
+  function executeTyped(address to, uint256 amount) public returns (bytes memory) {
+    return execute(abi.encode(to, amount));
   }
 }
