a1b85e583d6ffa91034d71d58b74350d9a9dad85
ashys <40616911+chlwys@users.noreply.github.com>
2023-04-23 16:32:49 +0900

    deposit/withdrw systems

--- packages/contracts/src/systems/ERC721DepositSystem.sol
+++ packages/contracts/src/systems/ERC721DepositSystem.sol
@@ -11,10 +11,10 @@ import { LibPet } from "libraries/LibPet.sol";
 import { KamiERC721 } from "tokens/KamiERC721.sol";
 import { ERC721ProxySystem, ID as ProxyID } from "systems/ERC721ProxySystem.sol";
 
-uint256 constant ID = uint256(keccak256("system.ERC721.Withdraw"));
+uint256 constant ID = uint256(keccak256("system.ERC721.Deposit"));
 
 // sets a pet game world => outside world
-contract ERC721WithdrawSystem is System {
+contract ERC721DepositSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
   function execute(bytes memory arguments) public returns (bytes memory) {
@@ -24,10 +24,10 @@ contract ERC721WithdrawSystem is System {
     require(LibPet.getAccount(components, petID) == accountID, "Pet: not urs");
 
     // checks
-    require(!LibPet.isUnrevealed(components, id), "Pet: unrevealed");
+    require(!LibPet.isInWorld(components, id), "Pet: unrevealed");
 
     // actions to be taken upon bridging out
-    LibPet.setState(components, petID, "721_EXTERNAL");
+    LibPet.setState(components, petID, "RESTING");
 
     return "";
   }
