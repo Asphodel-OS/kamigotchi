37f1b4ea8670d9fe966c2d2f17a6b357e7f3a316
jirach <acheron@asphodel.io>
2023-05-05 22:13:05 +0900

    register components, systems, expose api

--- packages/client/src/layers/network/api/admin.ts
+++ packages/client/src/layers/network/api/admin.ts
@@ -68,11 +68,15 @@ export function createAdminAPI(systems: any) {
     registerFood(2, 'Pom-Pom Fruit Candy', 100);
     registerFood(3, 'Gakki Cookie Sticks', 200);
 
+    // create revive registry items
+    registerRevive(1, 'Red Gakki Ribbon', 10);
+
     // set listings on global merchant
     createMerchant('hawker', 0);
     setListing('hawker', 1, 25, 0); // merchant, item index, buy price, sell price
     setListing('hawker', 2, 90, 0);
-    setListing('hawker', 3, 150, 0);
+    setListing('hawker', 3, 160, 0);
+    setListing('hawker', 4, 500, 0);
 
     // // create our hottie merchant ugajin. names are unique
     // createMerchant('ugajin', 13);
@@ -213,6 +217,15 @@ export function createAdminAPI(systems: any) {
     );
   }
 
+  // @dev add a revive item registry entry
+  function registerRevive(reviveIndex: number, name: string, health: number) {
+    return systems['system._Registry.Revive.Create'].executeTyped(
+      reviveIndex,
+      name,
+      health
+    );
+  }
+
   // @dev adds a trait in registry
   function registerTrait(
     index: number,
@@ -291,6 +304,15 @@ export function createAdminAPI(systems: any) {
     );
   }
 
+  // @dev update a revive item registry entry
+  function updateRegistryRevive(reviveIndex: number, name: string, health: number) {
+    return systems['system._Registry.Revive.Update'].executeTyped(
+      reviveIndex,
+      name,
+      health
+    );
+  }
+
   return {
     init,
     giveCoins,
@@ -322,6 +344,10 @@ export function createAdminAPI(systems: any) {
         create: registerModification,
         update: updateRegistryModification,
       },
+      revive: {
+        create: registerRevive,
+        update: updateRegistryRevive,
+      }
     },
     room: { create: createRoom },
   };
