8e31b783538e295ffa7d7699132694649e2996d3
aprl <40616911+chlwys@users.noreply.github.com>
2023-06-09 21:07:02 +0300

    updated test configs

--- packages/contracts/src/libraries/LibPet.sol
+++ packages/contracts/src/libraries/LibPet.sol
@@ -33,6 +33,9 @@ import { LibStat } from "libraries/LibStat.sol";
 // placeholders for config values
 string constant UNREVEALED_URI = "https://kamigotchi.nyc3.cdn.digitaloceanspaces.com/placeholder.gif";
 
+// placeholders for config values
+string constant UNREVEALED_URI = "https://kamigotchi.nyc3.cdn.digitaloceanspaces.com/placeholder.gif";
+
 library LibPet {
   using LibFPMath for int256;
 
