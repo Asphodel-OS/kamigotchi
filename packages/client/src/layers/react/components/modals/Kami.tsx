5d6e0059dac7eb857ee6122360b54c528e875c00
jirach <acheron@asphodel.io>
2023-07-17 15:02:29 -0400

    doot

--- packages/client/src/layers/react/components/modals/Kami.tsx
+++ packages/client/src/layers/react/components/modals/Kami.tsx
@@ -212,7 +212,7 @@ export function registerKamiModal() {
       }, [setVisibleModals, visibleModals]);
 
       return (
-        <ModalWrapperFull zindex={true} divName='kami' id='kamiModal'>
+        <ModalWrapperFull divName='kami' id='kamiModal' overlay>
           <TopButton style={{ pointerEvents: 'auto' }} onClick={hideModal}>
             X
           </TopButton>
