756113ec6c6e02180ab62a712d408e69fe25e4d0
jirach <acheron@asphodel.io>
2023-08-18 04:16:36 -0400

    reorganize image assets pt1/2

--- packages/client/src/constants/phaser/rooms/index.ts
+++ packages/client/src/constants/phaser/rooms/index.ts
@@ -1,34 +1,14 @@
 import {
-  vendingmachine,
-  mooringpost,
-  toriigate,
-  hollowstump,
-  gate,
-  shopdoor,
   cashregister,
   prayerwheels,
   bellshapeddevice,
   glassbox,
-  trashbag,
-  acompanybuilding,
-  abuildinglogo,
-  foxstatues,
-  chair,
-  cabinet,
   occultcircle,
   monolith,
-  junkmonitors,
-  junkvendingwall,
-  warningsign,
-  beetle1,
-  beetle2,
-  beetle3,
-  smallmushrooms,
   beetle4,
   centipedeandgrub,
   foresttrunk,
   termitemound,
-  poster,
   appleimac,
   businesspaperwork,
   smallwaterfall,
@@ -40,15 +20,6 @@ import {
 } from 'assets/images/objects';
 
 import {
-  room1,
-  room2,
-  room3,
-  room4,
-  room5,
-  room6,
-  room7,
-  room8,
-  room9,
   room10,
   room11,
   room12,
@@ -66,12 +37,22 @@ import { triggerDialogueModal } from 'layers/phaser/utils/triggerDialogueModal';
 import { triggerERC20BridgeModal } from 'layers/phaser/utils/triggerERC20BridgeModal';
 import { triggerERC721BridgeModal } from 'layers/phaser/utils/triggerERC721BridgeModal';
 import { triggerShopModal } from 'layers/phaser/utils/triggerShopModal';
-import { triggerPetMintModal } from 'layers/phaser/utils/triggerPetMintModal';
 import { triggerPetNamingModal } from 'layers/phaser/utils/triggerPetNamingModal';
 import { triggerNodeModal } from 'layers/phaser/utils/triggerNodeModal';
 import { triggerRoomMovementModal } from 'layers/phaser/utils/triggerRoomMovementModal';
 import { roomObjectsWorld1, roomObjectsWorld2 } from 'assets/images/room objects';
 import { triggerLeaderboardModal } from 'layers/phaser/utils/triggerLeaderboardModal';
+
+
+import { room1 } from './1_mistyriver';
+import { room2 } from './2_treetunnel';
+import { room3 } from './3_gate';
+import { room4 } from './4_junkyard';
+import { room5 } from './5_restricted';
+import { room6 } from './6_office-front';
+import { room7 } from './7_office-lobby';
+import { room8 } from './8_junkshop';
+import { room9 } from './9_forest';
 
 // NOTE: This is the most horrendous, hardcoded known to mankind. We should
 // move most things here to the store and populate the information from onchain.
@@ -109,278 +90,15 @@ export const duplicateRoomMusic = [
 // all our lovely, hardcoded room details
 export const rooms: Room[] = [
   { location: 0 },
-  {
-    location: 1,
-    background: {
-      key: 'bg_room001',
-      path: room1,
-    },
-    music: {
-      key: 'opening',
-      path: opening,
-    },
-    objects: [
-      {
-        key: 'mooringpost',
-        path: mooringpost,
-        offset: { x: -19, y: 38 },
-        onClick: () =>
-          triggerDialogueModal([
-            "This looks like a mooring post. There's enough rope attached to secure a boat. Somehow, you know the spot is taken.",
-          ]),
-      },
-    ],
-  },
-  {
-    location: 2,
-    background: {
-      key: 'bg_room002',
-      path: room2,
-    },
-    music: {
-      key: 'opening',
-      path: opening,
-    },
-    objects: [
-      {
-        key: 'hollowstump',
-        path: hollowstump,
-        offset: { x: -48.5, y: 29.5 },
-        onClick: () =>
-          triggerDialogueModal([
-            "It's a hollow tree stump. There doesn't appear to be anything inside.",
-          ]),
-      },
-      {
-        key: 'gate',
-        path: gate,
-        offset: { x: -39.5, y: -33.5 },
-        onClick: () => triggerDialogueModal(["There's some sort of gate in the distance."]),
-      },
-      {
-        key: 'shopdoor',
-        path: shopdoor,
-        offset: { x: 5, y: -7 },
-        onClick: () =>
-          triggerDialogueModal(["There's what appears to be a door hanging in mid-air!"]),
-      },
-    ],
-  },
-  {
-    location: 3,
-    background: {
-      key: 'bg_room003',
-      path: room3,
-    },
-    music: {
-      key: 'opening',
-      path: opening,
-    },
-    objects: [
-      {
-        key: 'toriigate',
-        path: toriigate,
-        offset: { x: 21, y: -8 },
-        onClick: triggerNodeModal,
-      },
-    ],
-  },
-  {
-    location: 4,
-    background: {
-      key: 'bg_room004',
-      path: room4,
-    },
-    music: {
-      key: 'forest',
-      path: forest,
-    },
-    objects: [
-      {
-        key: 'vendingmachine',
-        path: vendingmachine,
-        offset: { x: -33.5, y: 9.5 },
-        onClick: triggerPetMintModal,
-      },
-    ],
-  },
-  {
-    location: 5,
-    background: {
-      key: 'bg_room005',
-      path: room5,
-    },
-    music: {
-      key: 'ost1',
-      path: ost1,
-    },
-    objects: [
-      {
-        key: 'trashbag',
-        path: trashbag,
-        offset: { x: -55.5, y: 50 },
-        onClick: () => triggerDialogueModal(['A bag of trash. But rooting through it...']),
-      },
-      {
-        key: 'acompanybuilding',
-        path: acompanybuilding,
-        offset: { x: -30.1, y: -35 },
-        onClick: () => triggerDialogueModal(['An office building?']),
-      },
-      {
-        key: 'warningsign',
-        path: warningsign,
-        offset: { x: 10.5, y: 39.6 },
-        onClick: () =>
-          triggerDialogueModal([
-            'The "writing" on this sign is illegible nonsense. It looks like a warning, however.',
-          ]),
-      },
-    ],
-  },
-  {
-    location: 6,
-    background: {
-      key: 'bg_room006',
-      path: room6,
-    },
-    music: {
-      key: 'ost1',
-      path: ost1,
-    },
-    objects: [
-      {
-        key: 'abuildinglogo',
-        path: abuildinglogo,
-        offset: { x: 0, y: -45 },
-        onClick: () =>
-          triggerDialogueModal([
-            "There's a logo plaque here. It's perfectly maintained, despite the decrepit state of the rest of of the entrance.",
-          ]),
-      },
-      {
-        key: 'foxstatues',
-        path: foxstatues,
-        offset: { x: 0, y: 28 },
-        onClick: () =>
-          triggerDialogueModal([
-            'The fox statues are flanking the entrance perfectly. As you step past them, the texture of the air changes slightly.',
-          ]),
-      },
-    ],
-  },
-  {
-    location: 7,
-    background: {
-      key: 'bg_room007',
-      path: room7,
-    },
-    music: {
-      key: 'ost3',
-      path: ost3,
-    },
-    objects: [
-      {
-        key: 'chair',
-        path: chair,
-        offset: { x: -40, y: 31.9 },
-        onClick: () =>
-          triggerDialogueModal([
-            'While obviously aged, the couch is still in respectable condition.',
-          ]),
-      },
-      {
-        key: 'cabinet',
-        path: cabinet,
-        offset: { x: 26, y: 17.4 },
-        onClick: () => triggerDialogueModal(['A cabinet. Inside...']),
-      },
-    ],
-  },
-  {
-    location: 8,
-    background: {
-      key: 'bg_room008',
-      path: room8,
-    },
-    music: {
-      key: 'ost3',
-      path: ost3,
-    },
-    objects: [
-      {
-        key: 'junkmonitors',
-        path: junkmonitors,
-        offset: { x: 54, y: 17 },
-        onClick: () =>
-          triggerDialogueModal([
-            'These appear to be junked computer monitors. It looks like someone was working on them.',
-          ]),
-      },
-      {
-        key: 'junkvendingwall',
-        path: junkvendingwall,
-        offset: { x: -47.5, y: -4.5 },
-        onClick: () =>
-          triggerDialogueModal([
-            "A wall that vends junk. This is probably where you'll be able to get mods.",
-          ]),
-      },
-      {
-        key: 'poster',
-        path: poster,
-        offset: { x: 35.5, y: -1.4 },
-        onClick: () =>
-          triggerDialogueModal(['A poster of no particular importance. Possibly too much.']),
-      },
-    ],
-  },
-  {
-    location: 9,
-    background: {
-      key: 'bg_room009',
-      path: room9,
-    },
-    music: {
-      key: 'ost2',
-      path: ost2,
-    },
-    objects: [
-      {
-        key: 'beetle1',
-        path: beetle1,
-        offset: { x: 53.5, y: -53.35 },
-        onClick: () =>
-          triggerDialogueModal([
-            'The first of a number of local beetles. Quiet and contemplative.',
-          ]),
-      },
-      {
-        key: 'beetle2',
-        path: beetle2,
-        offset: { x: 11.5, y: -7 },
-        onClick: () =>
-          triggerDialogueModal([
-            'The second of a number of local beetles. While they might be insignificant to you, their numbers are very important to them.',
-          ]),
-      },
-      {
-        key: 'beetle3',
-        path: beetle3,
-        offset: { x: -59.5, y: -15.5 },
-        onClick: () => triggerDialogueModal(['Beetle number three. More private than the others.']),
-      },
-      {
-        key: 'smallmushrooms',
-        path: smallmushrooms,
-        offset: { x: -52, y: 58 },
-        onClick: () =>
-          triggerDialogueModal([
-            "You haven't seen Mushrooms like this anywhere else in this forest.",
-          ]),
-      },
-    ],
-  },
+  room1,
+  room2,
+  room3,
+  room4,
+  room5,
+  room6,
+  room7,
+  room8,
+  room9,
   {
     location: 10,
     background: {
