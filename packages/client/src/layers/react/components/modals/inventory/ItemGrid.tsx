7552e5c7d12a798b1dfc40e1dbcdf4c951c2a57e
aprl <40616911+chlwys@users.noreply.github.com>
2023-11-03 22:56:52 +0800

    itemIcon and reward screen

--- packages/client/src/layers/react/components/modals/inventory/ItemGrid.tsx
+++ packages/client/src/layers/react/components/modals/inventory/ItemGrid.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { Tooltip } from "layers/react/components/library/Tooltip";
+import { ItemIcon } from "layers/react/components/library/ItemIcon";
 import { Inventory } from "layers/react/shapes/Inventory";
 import { dataStore } from "layers/react/store/createStore";
 
@@ -18,38 +19,28 @@ export const ItemGrid = (props: Props) => {
   }
 
   const Cell = (inventory: Inventory) => {
-    const ImgButton = (inv: Inventory) => {
-      let foo = () => { };
-      let clickable = false;
-      switch (inv.item.type) {
-        case 'LOOTBOX':
-          foo = openLootbox;
-          clickable = true;
-          break;
-        default:
-          clickable = false;
-          break;
-      }
-
-      if (clickable) {
-        return (
-          <IconClickable src={inv.item.image.default} onClick={foo} />
-        );
-      } else {
-        return (
-          <Icon src={inv.item.image.default} />
-        );
-      }
+    let foo = () => { };
+    let clickable = false;
+    switch (inventory.item.type) {
+      case 'LOOTBOX':
+        foo = openLootbox;
+        clickable = true;
+        break;
+      default:
+        clickable = false;
+        break;
     }
 
-
     return (
-      <Tooltip key={inventory.id} text={[inventory.item.name]}>
-        <Slot>
-          {ImgButton(inventory)}
-          <Balance>{inventory.balance}</Balance>
-        </Slot>
-      </Tooltip>
+      <ItemIcon
+        key={`inventory-${inventory.item.index}`}
+        id={`inventory-${inventory.item.index}`}
+        item={inventory.item}
+        balance={inventory.balance}
+        size='fixed'
+        onClick={clickable ? foo : undefined}
+        named
+      />
     );
   }
 
@@ -65,48 +56,4 @@ const Container = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: flex-start;
-`;
-
-const Slot = styled.div`
-  position: relative;
-  border: solid black 1.5px;
-  border-radius: 4px;
-
-  margin: 5px;
-
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-`;
-
-const Icon = styled.img`
-  height: 50px;
-  width: 50px;
-  padding: 5px;
-`;
-
-const IconClickable = styled.img`
-  height: 50px;
-  width: 50px;
-  padding: .5vw;
-
-    &:hover {
-    background-color: #BBB;
-  }
-`;
-
-const Balance = styled.div` 
-  border-top: solid black 1.25px;
-  border-left: solid black 1.25px;
-  border-radius: 2.5px 0 0 0;
-  background-color: #FFF;
-
-  position: absolute;
-  color: black;
-  right: 0;
-  bottom: 0;
-  padding: 2px;
-
-  font-family: Pixel;
-  font-size: 8px;
 `;
