39729ab6140d2985bf188eb5769f306aa73dc35b
jirach <acheron@asphodel.io>
2024-02-14 01:45:38 -0700

    doot

--- packages/client/src/layers/phaser/scenes/GameScene.ts
+++ packages/client/src/layers/phaser/scenes/GameScene.ts
@@ -1,6 +1,7 @@
+import Phaser from 'phaser';
+
 import { backgrounds } from 'assets/images/backgrounds';
 import { Room } from 'constants/rooms';
-import Phaser from 'phaser';
 import { checkModalCoverage } from '../utils/checkModalCoverage';
 import { checkDuplicateRooms } from '../utils/rooms';
 import { triggerDialogueModal } from '../utils/triggers/triggerDialogueModal';
