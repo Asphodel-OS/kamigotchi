2fd8d205920233dfc34cf764e4c81b6cb1dcdd3d
jirach <acheron@asphodel.io>
2024-02-19 14:03:28 -0700

    some crayoning

--- packages/client/src/layers/react/components/modals/chat/Feed.tsx
+++ packages/client/src/layers/react/components/modals/chat/Feed.tsx
@@ -1,9 +1,10 @@
 import { NeynarAPIClient } from '@neynar/nodejs-sdk';
 import { CastWithInteractions, FeedResponse } from '@neynar/nodejs-sdk/build/neynar-api/v2';
 import moment from 'moment';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import { ActionButton } from '../../library';
+
+import { ActionButton, Tooltip } from '../../library';
 
 interface Props {
   client: NeynarAPIClient;
@@ -13,31 +14,43 @@ export const Feed = (props: Props) => {
   const { client } = props;
   const [feed, setFeed] = useState<FeedResponse>();
   const [casts, setCasts] = useState<CastWithInteractions[]>([]);
+  const feedRef = useRef<HTMLDivElement>(null);
 
-  const poll = async () => {
+  const poll = useCallback(async () => {
     const newFeed = await client.fetchFeed('filter', {
       filterType: 'channel_id',
       channelId: 'kamigotchi',
-      cursor: feed?.next.cursor || undefined,
-      limit: 2,
+      cursor: feed?.next.cursor ?? '',
+      limit: 5,
     });
     setFeed(newFeed);
 
-    const currCasts = casts;
+    const currCasts = [...casts];
     for (const cast of newFeed.casts) {
-      if (!currCasts.find((c) => c.hash === cast.hash)) {
-        currCasts.push(cast);
-      }
+      if (!currCasts.find((c) => c.hash === cast.hash)) currCasts.push(cast);
     }
     setCasts(currCasts);
-  };
+  }, [client, feed]);
 
   useEffect(() => {
     poll();
+    // const current = feedRef.current;
+    // const handleScroll = (oldCasts: CastWithInteractions[]) => {
+    //   if (feedRef.current && feedRef.current.scrollTop === 0) {
+    //     console.log('scrolltop: ', feedRef.current.scrollTop);
+    //     poll();
+    //   }
+    // };
+    //
+    // if (current) current.addEventListener('scroll', () => handleScroll(casts));
+    // return () => {
+    //   if (current) current.removeEventListener('scroll', () => handleScroll(casts));
+    // };
   }, []);
 
   useEffect(() => {
-    console.log('feed', feed);
+    console.log('feed casts', feed?.casts);
+    console.log('feed cursor', feed?.next.cursor);
   }, [feed]);
 
   useEffect(() => {
@@ -45,40 +58,37 @@ export const Feed = (props: Props) => {
   }, [casts]);
 
   return (
-    <Wrapper>
-      <ActionButton text='load more' id='load' onClick={poll} />
+    <Wrapper ref={feedRef}>
+      <Tooltip text={feed?.next.cursor ? ['load more'] : ['no more!']}>
+        <ActionButton text='load more' id='load' onClick={poll} disabled={!feed?.next.cursor} />
+      </Tooltip>
       {casts?.toReversed().map((cast) => (
-        <Message2>
+        <Message>
           <Pfp src={cast.author.pfp_url} />
-
-          <Message>
+          <Content>
             <Header>
               <Author>{cast.author.username}</Author>
-              <Time>{moment(cast.timestamp).format('YYYY-MM-DD HH:mm')}</Time>
+              <Time>{moment(cast.timestamp).format('MM/DD HH:mm')}</Time>
             </Header>
-            <Content>{cast.text}</Content>
-          </Message>
-        </Message2>
+            <Body>{cast.text}</Body>
+          </Content>
+        </Message>
       ))}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  padding: 0.5vw;
-  align-self: center;
   width: 100%;
 
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   overflow-y: scroll;
-
-  font-family: Pixel;
 `;
 
-const Message2 = styled.div`
-  padding: 0.9vw 0.6vw;
+const Message = styled.div`
+  padding: 0.9vw 0.9vw;
   width: 100%;
 
   color: black;
@@ -92,8 +102,9 @@ const Message2 = styled.div`
   }
 `;
 
-const Message = styled.div`
+const Content = styled.div`
   color: black;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -107,26 +118,32 @@ const Pfp = styled.img`
 `;
 
 const Header = styled.div`
-  padding-bottom: 0.6vw;
+  padding-bottom: 0.9vw;
+  width: 100%;
   color: black;
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
+  justify-content: space-between;
   gap: 0.4vw;
 `;
 
 const Author = styled.div`
   color: orange;
-  font-size: 1.2vw;
+  font-family: Pixel;
+  font-size: 1vw;
 `;
 
 const Time = styled.div`
   color: black;
-  font-size: 1vw;
+  font-family: Pixel;
+  font-size: 0.9vw;
 `;
 
-const Content = styled.div`
+const Body = styled.div`
   color: black;
-  font-size: 0.9vw;
+  width: 100%;
+  font-family: Pixel;
+  font-size: 0.8vw;
   line-height: 1.2vw;
 `;
