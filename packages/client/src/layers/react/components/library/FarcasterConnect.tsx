bcee200aaf42bc644fb544864abe53aea29d2372
jirach <acheron@asphodel.io>
2024-02-23 08:53:56 -0700

    clean up farcaster connection flow

--- packages/client/src/layers/react/components/library/FarcasterConnect.tsx
+++ packages/client/src/layers/react/components/library/FarcasterConnect.tsx
@@ -1,21 +1,21 @@
+import { EntityID } from '@latticexyz/recs';
+import crypto from 'crypto';
 import { useEffect } from 'react';
 import { useLocalStorage } from 'usehooks-ts';
 
 import { farcasterLogo } from 'assets/images/logos';
 import { Account } from 'layers/network/shapes/Account';
 import { IconButton, Tooltip } from 'layers/react/components/library';
-import { useAccount } from 'layers/react/store/account';
+import { useAccount, useNetwork } from 'layers/react/store';
 import { FarcasterUser, client, emptyFaracasterUser, handleSignIn } from 'src/clients/neynar';
 
 interface Props {
   account: Account;
-  actions: {
-    connectFarcaster: (fid: number, pfpURI: string) => void;
-  };
 }
 
 export const FarcasterConnect = (props: Props) => {
-  const { account, actions } = props;
+  const { account } = props;
+  const { selectedAddress, networks } = useNetwork();
   const [farcasterUser, setFarcasterUser] = useLocalStorage<FarcasterUser>(
     'farcasterUser',
     emptyFaracasterUser
@@ -24,11 +24,19 @@ export const FarcasterConnect = (props: Props) => {
 
   // update farcaster user in localstorage when the account store value changes
   useEffect(() => {
-    if (kamiAccount.fid && kamiAccount.neynar_signer) {
+    if (
+      kamiAccount.fid &&
+      kamiAccount.neynar_signer &&
+      (farcasterUser.fid != kamiAccount.fid ||
+        farcasterUser.signer_uuid !== kamiAccount.neynar_signer)
+    ) {
       console.log('updating farcaster id and/or signer');
-      updateFarcasterUser();
+      updateLocalStorageFUser();
     }
   }, [kamiAccount.fid, kamiAccount.neynar_signer]);
+
+  /////////////////
+  // INTERPRETATION
 
   const getColor = () => {
     if (!farcasterUser.fid) return 'orange';
@@ -47,12 +55,14 @@ export const FarcasterConnect = (props: Props) => {
 
   const getOnClick = () => {
     if (!farcasterUser.fid) return () => handleSignIn();
-    if (!account.fid)
-      return () => actions.connectFarcaster(farcasterUser.fid, farcasterUser.pfp_url);
+    if (!account.fid) return () => connectFarcaster(farcasterUser.fid, farcasterUser.pfp_url);
     if (farcasterUser.fid !== account.fid)
-      return () => actions.connectFarcaster(farcasterUser.fid, farcasterUser.pfp_url);
+      return () => connectFarcaster(farcasterUser.fid, farcasterUser.pfp_url);
     return () => {};
   };
+
+  /////////////////
+  // RENDERING
 
   return (
     <Tooltip text={getTooltipText()}>
@@ -61,7 +71,7 @@ export const FarcasterConnect = (props: Props) => {
   );
 
   // set the farcaster user in localstorage, based on the fid found in Account Store
-  async function updateFarcasterUser() {
+  async function updateLocalStorageFUser() {
     const fid = kamiAccount.fid!;
     const signer_uuid = kamiAccount.neynar_signer!;
     const response = await client.fetchBulkUsers([fid], {});
@@ -78,5 +88,24 @@ export const FarcasterConnect = (props: Props) => {
       console.log('setting farcaster user in localstorage', fUser);
       setFarcasterUser(fUser);
     }
+  }
+
+  function connectFarcaster(fid: number, pfpURI: string) {
+    const network = networks.get(selectedAddress);
+    if (!network) {
+      console.error(`Network not found for address ${selectedAddress}`);
+      return;
+    }
+
+    const actionID = crypto.randomBytes(32).toString('hex') as EntityID;
+    network.actions?.add({
+      id: actionID,
+      action: 'ConnectFarcaster',
+      params: [fid, pfpURI],
+      description: `Connecting to Farcaster Account ${fid}`,
+      execute: async () => {
+        return network.api.player.account.set.farcaster(fid, pfpURI);
+      },
+    });
   }
 };
