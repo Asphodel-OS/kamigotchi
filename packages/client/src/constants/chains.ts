c81afbf4e3a644f296416210feee7eb674e45073
jirach <acheron@asphodel.io>
2024-04-09 08:53:01 -0400

    rug unnecessary modes from chain config map

--- packages/client/src/constants/chains.ts
+++ packages/client/src/constants/chains.ts
@@ -7,12 +7,8 @@ const opSepoliaOverride = addRpcUrlOverrideToChain(
 );
 
 export const chainConfigs: Map<string, Chain> = new Map();
-chainConfigs.set('', localhost);
-chainConfigs.set('localhost', localhost);
 chainConfigs.set('development', localhost);
 chainConfigs.set('staging', opSepoliaOverride);
-chainConfigs.set('test', opSepoliaOverride);
-chainConfigs.set('OPSEP', opSepoliaOverride);
 chainConfigs.set('production', optimism);
 
 export const defaultChain = chainConfigs.get(import.meta.env.MODE ?? '')!;
