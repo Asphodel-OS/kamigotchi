930970fe4ba5fd03bb39e9a9459aa09671c8b22f
jirach <acheron@asphodel.io>
2024-05-01 18:32:28 -0400

    canvas clearout on logout

--- packages/client/src/layers/react/components/canvas/Room.tsx
+++ packages/client/src/layers/react/components/canvas/Room.tsx
@@ -20,13 +20,12 @@ export const Room = (props: Props) => {
   // in the Volume Settings modal. This recreates any new music from scratch,
   // but ideally we should keep all played tracks in a state map for reuse.
   useEffect(() => {
-    console.log('Room index detected', index);
-    if (!index || index == room.roomIndex) return;
-    console.log('Switching to room', index);
+    if (index == room.roomIndex) return;
     const newRoom = rooms[index];
 
     const music = newRoom.music;
-    if (music && music.path !== room.music?.path) {
+    if (!music) bgm?.stop();
+    else if (music.path !== room.music?.path) {
       const newBgm = new Howl({ src: [music.path], loop: true });
       if (bgm) bgm.stop();
       newBgm.play();
