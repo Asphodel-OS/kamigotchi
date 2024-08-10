0a118e889602772b82939ab79ccfe2506b779bff
jirach <acheron@asphodel.io>
2024-08-10 16:01:46 +0100

    just query for the whole damn thing..

--- packages/client/src/app/components/modals/quests/Quests.tsx
+++ packages/client/src/app/components/modals/quests/Quests.tsx
@@ -1,12 +1,11 @@
-import { EntityID, EntityIndex } from '@mud-classic/recs';
+import { EntityID } from '@mud-classic/recs';
 import { useEffect, useState } from 'react';
 import { interval, map } from 'rxjs';
 
 import { ModalHeader, ModalWrapper } from 'app/components/library';
 import { registerUIComponent } from 'app/root';
-import { useVisibility } from 'app/stores';
 import { questsIcon } from 'assets/images/icons/menu';
-import { Account, getAccountFromBurner } from 'network/shapes/Account';
+import { getAccountFromBurner } from 'network/shapes/Account';
 import {
   Quest,
   filterQuestsByAvailable,
@@ -17,7 +16,6 @@ import {
   parseQuestStatuses,
 } from 'network/shapes/Quest';
 import { getDescribedEntity } from 'network/shapes/utils/parse';
-import { waitForActionCompletion } from 'network/utils';
 import { Footer } from './Footer';
 import { List } from './List';
 import { Tabs } from './Tabs';
@@ -47,45 +45,45 @@ export function registerQuestsModal() {
           // NOTE(jb): ideally we only update when these shapes change but for
           // the time being we'll update on every tick to force a re-render.
           // just separating these out to flatten our Account shapes
-          // TODO (jb): move inside effect hook once we have proper Objective/Requirements tracking
+          // TODO: move inside effect hook once we have proper Objective/Requirements tracking
+          const registryQuests = getRegistryQuests(world, components);
+          const registryParsed = parseQuestStatuses(world, components, account, registryQuests);
           const ongoingQuests = getOngoingQuests(world, components, account.id);
           const completedQuests = getCompletedQuests(world, components, account.id);
           const ongoingParsed = parseQuestStatuses(world, components, account, ongoingQuests);
           const completedParsed = parseQuestStatuses(world, components, account, completedQuests);
+          const availableQuests = filterQuestsByAvailable(
+            registryParsed,
+            completedParsed,
+            ongoingParsed
+          );
 
           return {
             network,
             data: {
               account,
+              registry: registryParsed,
+              available: availableQuests,
               ongoing: ongoingParsed,
               completed: completedParsed,
-              registry: getRegistryQuests(world, components),
             },
           };
         })
       ),
     ({ network, data }) => {
       const { actions, api, components, notifications, world } = network;
-      const { account, completed, ongoing, registry } = data;
+      const { account, available, completed, ongoing, registry } = data;
       const [tab, setTab] = useState<TabType>('ONGOING');
-      const { modals } = useVisibility();
-      const [available, setAvailable] = useState<Quest[]>([]);
 
       /////////////////
       // SUBSCRIPTIONS
-
-      // update the State-based (Parsed) Quest Registry when we detect a change
-      // in the Props-based (UnParsed) Quest Registry. recheck the number of
-      // available quests for the Notification bar as well
-      useEffect(() => {
-        refreshAvailable(account, registry, completed, ongoing);
-      }, [registry.length, modals.quests]);
 
       // update the Notifications when the number of available quests changes
       // Q(jb): do we want this in a react component or on an independent hook?
       useEffect(() => {
         const id = 'Available Quests';
         const numAvail = available.length;
+        if (available.length > 0) setTab('AVAILABLE');
 
         if (notifications.has(id as EntityID)) {
           if (numAvail == 0) notifications.remove(id as EntityID);
@@ -108,30 +106,29 @@ export function registerQuestsModal() {
         }
       }, [available.length]);
 
-      const refreshAvailable = (
-        account: Account,
-        registry: Quest[],
-        completed: Quest[],
-        ongoing: Quest[]
-      ) => {
-        const parsedRegistry = parseQuestStatuses(world, components, account, registry);
-        const availableQuests = filterQuestsByAvailable(parsedRegistry, completed, ongoing);
-        if (availableQuests.length > 0) setTab('AVAILABLE');
-        setAvailable(availableQuests);
-      };
+      // const refreshAvailable = (
+      //   account: Account,
+      //   registry: Quest[],
+      //   completed: Quest[],
+      //   ongoing: Quest[]
+      // ) => {
+      //   const parsedRegistry = parseQuestStatuses(world, components, account, registry);
+      //   const availableQuests = filterQuestsByAvailable(parsedRegistry, completed, ongoing);
+      //   setAvailable(availableQuests);
+      // };
 
-      const refreshAfterAction = async (actionIndex: EntityIndex) => {
-        await waitForActionCompletion(actions!.Action, actionIndex);
-        setTimeout(() => {
-          refreshAvailable(account, registry, completed, ongoing);
-        }, REFRESH_PERIOD + 500);
-      };
+      // const refreshAfterAction = async (actionIndex: EntityIndex) => {
+      //   await waitForActionCompletion(actions!.Action, actionIndex);
+      //   setTimeout(() => {
+      //     refreshAvailable(account, registry, completed, ongoing);
+      //   }, REFRESH_PERIOD + 500);
+      // };
 
       /////////////////
       // ACTIONS
 
       const acceptQuest = async (quest: Quest) => {
-        const actionIndex = actions.add({
+        actions.add({
           action: 'QuestAccept',
           params: [quest.index * 1],
           description: `Accepting Quest ${quest.index * 1}`,
@@ -139,11 +136,10 @@ export function registerQuestsModal() {
             return api.player.quests.accept(quest.index);
           },
         });
-        refreshAfterAction(actionIndex);
       };
 
       const completeQuest = async (quest: Quest) => {
-        const actionIndex = actions.add({
+        actions.add({
           action: 'QuestComplete',
           params: [quest.id],
           description: `Completing Quest ${quest.index * 1}`,
@@ -151,10 +147,8 @@ export function registerQuestsModal() {
             return api.player.quests.complete(quest.id);
           },
         });
-        refreshAfterAction(actionIndex);
       };
 
-      if (!modals.quests) return <></>;
       return (
         <ModalWrapper
           id='quests'
