79c85ae4a62bf2645965d09c220190c29d3d3a72
Xenofluxx <susanainu@hotmail.com>
2024-10-17 22:48:18 +0200

    IconList and IconListButton refactor

--- packages/client/src/app/components/library/base/buttons/IconButton.tsx
+++ packages/client/src/app/components/library/base/buttons/IconButton.tsx
@@ -5,95 +5,165 @@ import { playClick } from 'utils/sounds';
 
 interface Props {
   img: string;
-  onClick: Function;
+  scalesOnHeight?: boolean;
+  onClick: () => void;
   text?: string;
   size?: number;
   color?: string;
   disabled?: boolean;
   noMargin?: boolean;
   pulse?: boolean;
+  balance?: number;
+  fullWidth?: boolean;
+  isIconList?: boolean;
 }
 
 // ActionButton is a text button that triggers an Action when clicked
 export const IconButton = (props: Props) => {
-  const { img, onClick, text, size, disabled, color, pulse, noMargin } = props;
+  const {
+    img,
+    onClick,
+    text,
+    size,
+    color,
+    disabled,
+    noMargin,
+    pulse,
+    balance,
+    scalesOnHeight,
+    fullWidth,
+    isIconList,
+  } = props;
   const scale = size ?? 2.5;
+  const scalescaleOrientation = scalesOnHeight ? 'vh' : 'vw';
 
   // layer on a sound effect
   const handleClick = async () => {
     playClick();
     await onClick();
   };
-
-  // override styles for sizes and disabling
-  const setStyles = () => {
-    let styles: any = {};
-    if (color) styles.backgroundColor = color;
-    if (disabled) styles.backgroundColor = '#b2b2b2';
-
-    return styles;
-  };
-
+  //console.log('im text' + text);
   return (
     <Button
       scale={scale}
-      onClick={!disabled ? handleClick : () => {}}
-      style={setStyles()}
+      onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+        !disabled ? handleClick() : e.preventDefault;
+      }}
       color={color ?? '#fff'}
       pulse={pulse}
       noMargin={noMargin}
       disabled={disabled}
       square={!text}
+      fullWidth={fullWidth}
+      isIconList={isIconList}
     >
-      <Image src={img} scale={scale} />
-      {text && <Text scale={scale}>{text}</Text>}
+      {balance ? <Balance>{balance}</Balance> : isIconList && <Corner />}
+      <Image isIconList={isIconList} src={img} scale={scale} orientation={scalescaleOrientation} />
+      {text && (
+        <Text isIconList={isIconList} scale={scale}>
+          {text}
+        </Text>
+      )}
     </Button>
   );
 };
 
-interface ButtonProps {
+const Button = styled.button<{
   scale: number;
   color: string;
   disabled?: boolean;
   pulse?: boolean;
   noMargin?: boolean;
   square?: boolean;
-}
-
-const Button = styled.button<ButtonProps>`
+  fullWidth?: boolean;
+  isIconList?: boolean;
+  text?: string;
+}>`
   border: solid black 0.15vw;
-  border-radius: ${({ scale }) => scale * 0.2}vw;
-  height: ${({ scale }) => scale}vw;
-  ${({ square, scale }) => square && `width: ${scale}vw;`}
-
-  margin: ${({ scale, noMargin }) => (noMargin ? 0 : scale * 0.1)}vw;
-  padding: ${({ scale }) => scale * 0.1}vw;
-  gap: ${({ scale }) => scale * 0.1}vw;
-
   display: flex;
-  flex-flow: row nowrap;
   justify-content: center;
   align-items: center;
-
   background-color: ${({ color, disabled }) => (disabled ? '#bbb' : color)};
   cursor: ${({ disabled }) => (disabled ? 'help' : 'pointer')};
   pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
-  user-select: none;
   &:hover {
     animation: ${() => hoverFx()} 0.2s;
     transform: scale(1.05);
   }
-  &:active {
-    animation: ${() => clickFx()} 0.3s;
-  }
+  ${({ isIconList, fullWidth, scale, noMargin, square, pulse }) => {
+    if (isIconList)
+      return `
+      position: relative;
+      border-radius: 0.45vw;
+      color: black;
+      width: ${fullWidth ? '100%' : 'auto'};
+      padding: 0.4vw;
+      gap: 0.4vw;
+    `;
+    if (!isIconList)
+      return `
+      margin: ${noMargin ? 0 : scale * 0.1}vw;
+      padding: ${scale * 0.1}vw;
+      gap: ${scale * 0.1}vw;
+      border-radius: ${scale * 0.2}vw;
+      height: ${scale}vw;
+      ${square && `width: ${scale}vw;`}
+      flex-flow: row nowrap;
+      user-select: none;
+      &:active {
+        animation: ${() => clickFx()} 0.3s;
+      }
 
-  animation: ${({ pulse }) => pulse && pulseFx} 3s ease-in-out infinite;
+      animation: ${pulse && pulseFx} 3s ease-in-out infinite;
+        `;
+  }}
 `;
 
-const Image = styled.img<{ scale: number }>`
-  height: ${({ scale }) => scale * 0.75}vw;
+const Text = styled.div<{ scale: number; isIconList?: boolean }>`
+  ${({ isIconList, scale }) => {
+    if (isIconList)
+      return `
+        font-size: 0.8vw;
+    `;
+    if (!isIconList)
+      return `
+        font-size: ${scale * 0.25}vw;
+    `;
+  }}
 `;
-
-const Text = styled.div<{ scale: number }>`
-  font-size: ${({ scale }) => scale * 0.25}vw;
+const Image = styled.img<{ scale: number; orientation: string; isIconList?: boolean }>`
+  ${({ isIconList, scale, orientation }) => {
+    if (isIconList)
+      return `
+        width: ${scale + orientation} ;
+        height: ${scale + orientation} ;
+        ${scale > 3.2 ? 'image-rendering: pixelated;' : ''};
+    `;
+    if (!isIconList)
+      return `
+        height: ${scale * 0.75}vw;
+    `;
+  }}
+`;
+const Balance = styled.div`
+  position: absolute;
+  background-color: white;
+  border-top: solid black 0.15vw;
+  border-left: solid black 0.15vw;
+  border-radius: 0.3vw 0 0.3vw 0;
+  bottom: 0;
+  right: 0;
+  font-size: 0.75vw;
+  align-items: center;
+  justify-content: center;
+  padding: 0.2vw;
+`;
+const Corner = styled.div`
+  position: absolute;
+  border: solid black 0.3vw;
+  border-color: transparent black black transparent;
+  right: 0;
+  bottom: 0;
+  width: 0;
+  height: 0;
 `;
--- packages/client/src/app/components/library/base/buttons/IconListButton.tsx
+++ packages/client/src/app/components/library/base/buttons/IconListButton.tsx
@@ -1,9 +1,10 @@
 import { Popover } from '@mui/material';
-import React, { useRef, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
-import { hoverFx } from 'app/styles/effects';
+import React from 'react';
 import { playClick } from 'utils/sounds';
+import { IconButton } from './IconButton';
 
 interface Props {
   img: string;
@@ -18,39 +19,30 @@ interface Props {
 
 export interface Option {
   text: string;
-  onClick: Function;
+  onClick: () => void;
   image?: string;
   disabled?: boolean;
 }
 
-export function IconListButton(props: Props) {
-  const { img, options, text, balance } = props;
-  const { disabled, fullWidth } = props;
-  const toggleRef = useRef<HTMLButtonElement>(null);
+export const IconListButton = (props: Props) => {
+  const { img, options, balance, fullWidth, disabled, scalesOnHeight, text } = props;
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
   const scale = props.scale ?? 1.4;
-  const scaleOrientation = props.scalesOnHeight ? 'vh' : 'vw';
-
-  const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (!disabled) {
-      playClick();
-      setAnchorEl(event.currentTarget);
-    }
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   // close the menu and layer in a sound effect
   const onSelect = (option: Option) => {
     playClick();
     option.onClick();
-    handleClose();
+    handleClick();
   };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  function handleClick(event?: React.MouseEvent<HTMLButtonElement>) {
+    if (!disabled) {
+      playClick();
+      event && !anchorEl ? setAnchorEl(event.currentTarget) : setAnchorEl(null);
+    }
+  }
 
   const MenuItem = (option: Option, i: number) => {
     return (
@@ -63,89 +55,38 @@ export function IconListButton(props: Props) {
 
   return (
     <Wrapper>
-      <Button ref={toggleRef} onClick={handleOpen} disabled={!!disabled} fullWidth={!!fullWidth}>
-        {balance ? <Balance>{balance}</Balance> : <Corner />}
-        <Image src={img} scale={scale} orientation={scaleOrientation} />
-        {text && <Text>{text}</Text>}
-      </Button>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+      <div
+        onClick={(e: React.MouseEvent<any>) => {
+          handleClick(e);
+        }}
       >
-        <Menu>{options.map((option, i) => MenuItem(option, i))}</Menu>
-      </Popover>
+        <IconButton
+          balance={balance}
+          scalesOnHeight={scalesOnHeight}
+          img={img}
+          disabled={!!disabled}
+          fullWidth={!!fullWidth}
+          onClick={handleClick}
+          isIconList={true}
+          text={text}
+          size={scale}
+        />
+
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        >
+          <Menu>{options.map((option, i) => MenuItem(option, i))}</Menu>
+        </Popover>
+      </div>
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div`
   width: auto;
-`;
-
-interface ButtonProps {
-  disabled: boolean;
-  fullWidth: boolean;
-}
-
-const Button = styled.button<ButtonProps>`
-  position: relative;
-  border: solid black 0.15vw;
-  border-radius: 0.45vw;
-  color: black;
-
-  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
-  padding: 0.4vw;
-  gap: 0.4vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background-color: ${({ disabled }) => (disabled ? '#bbb' : '#fff')};
-  cursor: ${({ disabled }) => (disabled ? 'help' : 'pointer')};
-  pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
-
-  &:hover {
-    animation: ${() => hoverFx()} 0.2s;
-    transform: scale(1.05);
-  }
-`;
-
-const Image = styled.img<{ scale: number; orientation: string }>`
-  width: ${({ scale }) => scale}${({ orientation }) => orientation};
-  height: ${({ scale }) => scale}${({ orientation }) => orientation};
-  ${({ scale }) => (scale > 3.2 ? 'image-rendering: pixelated;' : '')}
-`;
-
-const Text = styled.div`
-  font-size: 0.8vw;
-`;
-
-const Corner = styled.div`
-  position: absolute;
-  border: solid black 0.3vw;
-  border-color: transparent black black transparent;
-  right: 0;
-  bottom: 0;
-  width: 0;
-  height: 0;
-`;
-
-const Balance = styled.div`
-  position: absolute;
-  background-color: white;
-  border-top: solid black 0.15vw;
-  border-left: solid black 0.15vw;
-  border-radius: 0.3vw 0 0.3vw 0;
-  bottom: 0;
-  right: 0;
-
-  font-size: 0.75vw;
-  align-items: center;
-  justify-content: center;
-  padding: 0.2vw;
 `;
 
 const Menu = styled.div`
@@ -159,16 +100,13 @@ const MenuOption = styled.div<{ disabled?: boolean }>`
   display: flex;
   align-items: center;
   gap: 0.4vw;
-
   border-radius: 0.4vw;
   padding: 0.6vw;
   justify-content: left;
   font-size: 0.8vw;
-
   cursor: ${({ disabled }) => (disabled ? 'none' : 'pointer')};
   pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
   background-color: ${({ disabled }) => (disabled ? '#bbb' : '#fff')};
-
   &:hover {
     background-color: #ddd;
   }
