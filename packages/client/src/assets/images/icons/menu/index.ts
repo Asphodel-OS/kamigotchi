e9cf89f54b35a0a3766826d9875c8f2f299cb36c
Xenofluxx <susanainu@hotmail.com>
2024-10-24 21:43:11 +0200

    changed morebutton icon to .png

--- packages/client/src/assets/images/icons/menu/index.ts
+++ packages/client/src/assets/images/icons/menu/index.ts
@@ -3,7 +3,7 @@ import helpIcon from './help.png';
 import inventoryIcon from './inventory.png';
 import kamiIcon from './kami.png';
 import mapIcon from './map.png';
-import moreIcon from './more_icon_native.webp';
+import moreIcon from './more_icon_native.png';
 import operatorIcon from './operator.png';
 import questsIcon from './quests.png';
 import resetIcon from './reset_icon_native.png';
