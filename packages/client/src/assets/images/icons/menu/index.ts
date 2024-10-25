98e0523db71973363b5c8b65cc2a0957fdafe865
jirach <acheron@asphodel.io>
2024-10-25 04:48:30 -0400

    update file names

--- packages/client/src/assets/images/icons/menu/index.ts
+++ packages/client/src/assets/images/icons/menu/index.ts
@@ -3,10 +3,10 @@ import helpIcon from './help.png';
 import inventoryIcon from './inventory.png';
 import kamiIcon from './kami.png';
 import mapIcon from './map.png';
-import moreIcon from './more_icon_native.png';
+import moreIcon from './more.png';
 import operatorIcon from './operator.png';
 import questsIcon from './quests.png';
-import resetIcon from './reset_icon_native.png';
+import resetIcon from './reset.png';
 import settingsIcon from './settings.png';
 import socialIcon from './social.png';
 
