d5e7562acb5075ed44d9d229c669be66fab7f5b0
jirach <acheron@asphodel.io>
2024-11-18 04:01:03 -0500

    Revert "remove isUpdating on walletconnect (#1255)"
    This reverts commit dc7b4989444da564b123bee5529e567b74a7f9f3.

--- packages/client/src/app/components/validators/WalletConnector.tsx
+++ packages/client/src/app/components/validators/WalletConnector.tsx
@@ -120,11 +120,10 @@ export function registerWalletConnecter() {
         const embeddedWallet = getEmbeddedWallet(wallets);
         if (isUpdating || !injectedWallet || !embeddedWallet) return;
 
-        // setIsUpdating(true);
-        // await new Promise((resolve) => setTimeout(resolve, 2000));
+        setIsUpdating(true);
         await addNetworkAPI(injectedWallet);
         await updateBaseNetwork(embeddedWallet);
-        // setIsUpdating(false);
+        setIsUpdating(false);
       };
 
       // update the network store with the injected wallet's api
