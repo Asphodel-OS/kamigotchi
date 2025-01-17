b1219187b3382ab0666472fc382ff85dd7d3005e
jirach <acheron@asphodel.io>
2025-01-17 08:05:14 -0500

    contract fixes

--- packages/contracts/src/libraries/utils/LibCurve.sol
+++ packages/contracts/src/libraries/utils/LibCurve.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import { LibString } from "solady/utils/LibString.sol";
 import { SD59x18 } from "prb-math/SD59x18.sol";
 import { UD60x18 } from "prb-math/UD60x18.sol";
 import { LibFPConverter } from "libraries/utils/LibFPConverter.sol";
 
 struct GDAParams {
-  uint256 targetPrice;
-  uint256 startTs;
-  int256 scale;
-  int256 decay;
-  uint256 prevSold;
-  uint256 quantity;
+  uint256 targetPrice; // 1e0 precision
+  uint256 startTs; // 1e0 precision
+  int256 scale; // 1e18 precision
+  int256 decay; // 1e18 precision
+  uint256 prevSold; // 1e0 precision
+  uint256 quantity; // 1e0 precision (amt to be sold)
 }
 
 /// @notice an assortment of curve calculations
@@ -22,21 +23,15 @@ library LibCurve {
   using LibFPConverter for uint256;
 
   /// @notice calculate discrete GDA for a given set of parameters
-  /// @param params (noted below)
-  /// - targetPrice (1e0) target price
-  /// - startTs (1e0) timestamp of auction start
-  /// - scale (1e18) scale factor
-  /// - decay (1e18) decay constant
-  /// - prevSold (1e0) previous amount sold
-  /// - quantity (1e0) amount to be sold
+  /// @param params (noted in GDAParams struct)
   /// @return (1e18) total cost
   function calcGDA(GDAParams memory params) public view returns (int256) {
     SD59x18 qDelta = params.quantity.rawToSD();
     SD59x18 qInitial = params.prevSold.rawToSD();
     SD59x18 pTarget = params.targetPrice.rawToSD();
     SD59x18 tDelta = block.timestamp.rawToSD() - params.startTs.rawToSD();
-    SD59x18 scaleFactor = params.scale.rawToSD();
-    SD59x18 decayConstant = params.decay.rawToSD();
+    SD59x18 scaleFactor = params.scale.wadToSD();
+    SD59x18 decayConstant = params.decay.wadToSD();
 
     SD59x18 num1 = pTarget.mul(scaleFactor.pow(qInitial));
     SD59x18 num2 = scaleFactor.pow(qDelta) - int256(1).rawToSD();
