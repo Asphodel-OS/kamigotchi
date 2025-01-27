47e008f263cf3740a6044ec16d6d23be6a89e751
hubur <hubur@asphodel.io>
2025-01-27 15:32:37 +0100

    Single sub instance

--- packages/client/src/app/components/modals/chat/Chat.tsx
+++ packages/client/src/app/components/modals/chat/Chat.tsx
@@ -58,65 +58,21 @@ export function registerChatModal() {
 
       useEffect(() => {
         console.log('Chat visibility changed:', modals.chat);
-        /*
-        if (!modals.chat) return;
-
-        // Initialize Kamiden client and fetch initial messages
-        const initKamiden = async () => {
-          try {
-            console.log('Initializing Kamiden client');
-            const client = getKamidenClient();
-
-            // Get initial messages for room 0
-            console.log(`Fetching room messages from room lab entrance roomIndex 6`); // ${account.roomIndex}...`);
-
-            try {
-              const response = await client.getRoomMessages({ RoomIndex: 6 });
-              console.log('Messages', response.Messages);
-              setKamidenMessages(response.Messages);
-            } catch (e) {
-              console.log('Error fetching initial messages:', e);
-            }
-          } catch (error) {
-            console.error('Error connecting to Kamiden:', error);
-          }
-        };
-
-        initKamiden();
-        */
         return () => {
           console.log('Cleaning up Kamiden connection - chat closed');
         };
       }, [modals.chat]);
 
       const pushCast = (cast: CastWithInteractions) => {
-        //setCasts([cast, ...casts]);
       };
 
       const pushCasts = (newCasts: CastWithInteractions[]) => {
-        /*const oldCasts = [...casts];
-
-        // split the new casts into unique and duplicates
-        const uniqueCasts = [];
-        for (const [_, newCast] of newCasts.entries()) {
-          const collisionIndex = oldCasts.findIndex((c) => c.hash === newCast.hash);
-          if (collisionIndex != -1) oldCasts[collisionIndex] = newCast;
-          else uniqueCasts.push(newCast);
-        }
-
-        // sort the full set of casts by timestamp
-        const allCasts = [...uniqueCasts, ...oldCasts];
-        allCasts.sort((a, b) => moment(b.timestamp).diff(moment(a.timestamp)));
-        setCasts(allCasts);
-        */
       };
 
       const pushMessage = (newMessage: KamiMessage) => {
-        //setMessages([newMessage, ...messages]);
       };
 
       const pushMessages = (newMessages: KamiMessage[]) => {
-        //setKamidenMessages()
       };
 
       return (
