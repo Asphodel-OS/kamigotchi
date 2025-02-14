f96bccdce51285a333b13607f94736f51635a03d
jirach <acheron@asphodel.io>
2025-02-14 06:42:52 -0500

    autovisualize more kamis in pool on scroll

--- packages/client/src/app/components/modals/gacha/display/KamiBlock.tsx
+++ packages/client/src/app/components/modals/gacha/display/KamiBlock.tsx
@@ -1,32 +1,35 @@
 import styled from 'styled-components';
 
-import { Overlay } from 'app/components/library/styles';
+import { Overlay, Tooltip } from 'app/components/library';
 import { GachaKami } from 'network/shapes/Kami';
 
 interface Props {
   kami: GachaKami;
   onClick?: () => void;
+  tooltip: string[];
 }
 
 export const KamiBlock = (props: Props) => {
-  const { kami } = props;
+  const { tooltip, kami, onClick } = props;
   const { index, level, name } = kami;
 
   return (
     <Container>
-      <Image src={kami.image} onClick={props.onClick} />
-      <Overlay top={0.9} left={0.7}>
-        <Grouping>
-          <Text size={0.6}>Lvl</Text>
-          <Text size={0.6}>{level}</Text>
-        </Grouping>
-      </Overlay>
-      <Overlay top={0.9} right={0.7}>
-        <Text size={0.6}>{index}</Text>
-      </Overlay>
-      <Overlay bottom={0.6} fullWidth>
-        <Text size={0.6}>{name}</Text>
-      </Overlay>
+      <Tooltip key={kami.index} text={tooltip}>
+        <Image src={kami.image} onClick={onClick} />
+        <Overlay top={0.9} left={0.7}>
+          <Grouping>
+            <Text size={0.6}>Lvl</Text>
+            <Text size={0.6}>{level}</Text>
+          </Grouping>
+        </Overlay>
+        <Overlay top={0.9} right={0.7}>
+          <Text size={0.6}>{index}</Text>
+        </Overlay>
+        <Overlay bottom={0.6} fullWidth>
+          <Text size={0.6}>{name}</Text>
+        </Overlay>
+      </Tooltip>
     </Container>
   );
 };
@@ -34,7 +37,6 @@ export const KamiBlock = (props: Props) => {
 const Container = styled.div`
   background-color: white;
   border-radius: 0.6vw;
-  height: 10vw;
   margin: 0.9vw;
   filter: drop-shadow(0.2vw 0.2vw 0.1vw black);
 `;
@@ -42,7 +44,7 @@ const Container = styled.div`
 const Image = styled.img<{ onClick?: () => void }>`
   border: solid black 0.15vw;
   border-radius: 0.6vw;
-  height: 100%;
+  width: 10vw;
   image-rendering: pixelated;
 
   cursor: ${({ onClick }) => (onClick ? 'pointer' : 'inherit')};
