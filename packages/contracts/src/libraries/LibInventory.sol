472d744282e9c01a546dcdb838f0aa92633352a8
aprl <40616911+chlwys@users.noreply.github.com>
2025-03-19 17:37:32 +0900

    update gacha ticket index

--- packages/contracts/src/libraries/LibInventory.sol
+++ packages/contracts/src/libraries/LibInventory.sol
@@ -23,7 +23,7 @@ import { LibStat } from "libraries/LibStat.sol";
 // CONSTANTS
 
 uint32 constant MUSU_INDEX = 1;
-uint32 constant GACHA_TICKET_INDEX = 2;
+uint32 constant GACHA_TICKET_INDEX = 10;
 
 // handles nonfungible inventory instances
 library LibInventory {
