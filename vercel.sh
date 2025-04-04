b24418385376262bcff03bb73e6c3ca5f9d630e7
jirach <acheron@asphodel.io>
2025-04-03 21:46:48 -0400

    add "staging" build script

--- vercel.sh
+++ vercel.sh
@@ -3,7 +3,7 @@
 if [[ $MODE == "production" ]]; then 
   pnpm -F client build:prod
 elif [[ $MODE == "staging" ]]; then
-  pnpm -F client build:prod
+  pnpm -F client build:prev
 elif [[ $MODE == "testing" ]]; then
   pnpm -F client build:test
 else
