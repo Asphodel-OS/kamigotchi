66173bd3cf500271163037db76c6981cd3ae14f7
Xenofluxx <susanainu@hotmail.com>
2025-04-25 15:42:44 +0200

    add items

--- packages/client/src/assets/images/items/index.ts
+++ packages/client/src/assets/images/items/index.ts
@@ -1,5 +1,7 @@
 import agency_giftbox from './agency_giftbox.png';
 import astrolabe_disk from './astrolabe_disk.png';
+import astrolabe_part2 from './astrolabe_part2.png';
+import astrolabe_part3 from './astrolabe_part3.png';
 import best_ice_cream from './best_ice_cream.png';
 import better_ice_cream from './better_ice_cream.png';
 import black_poppy from './black_poppy.png';
@@ -8,7 +10,11 @@ import bless_potion from './bless_potion.png';
 import candle from './candle.png';
 import cheeseburger from './cheeseburger.png';
 import daffodil from './daffodil.png';
+import disc_shaped_map_native from './disc_shaped_map_native.png';
 import essence_of_daffodil from './essence_of_daffodil.png';
+import eth from './eth.png';
+import field_of_flowers_card_native from './field_of_flowers_card_native.png';
+import flower_bud_card_native from './flower_bud_card_native.png';
 import gacha_ticket from './gacha_ticket.png';
 import gakki_cookie_sticks from './gakki_cookie_sticks.png';
 import gaokerena_mochi from './gaokerena_mochi.png';
@@ -16,11 +22,14 @@ import gingerbread_cookie from './gingerbread_cookie.png';
 import glass_jar from './glass_jar.png';
 import grace_potion from './grace_potion.png';
 import greater_xp_potion from './greater_xp_potion.png';
+import heroic_awakening_card_native from './heroic_awakening_card_native.png';
 import holy_dust from './holy_dust.png';
 import ice_cream from './ice_cream.png';
 import initia_icon from './initia_icon.png';
 import kami_mochi from './kami_mochi.png';
 import kamigotchi_world_passport from './kamigotchi_world_passport.png';
+import lily_sprout_card_native from './lily_sprout_card_native.png';
+import lotus_bud_card_native from './lotus_bud_card_native.png';
 import mana_mochi from './mana_mochi.png';
 import maple_flavor_ghost_gum from './maple_flavor_ghost_gum.png';
 import microplastics from './microplastics.png';
@@ -28,6 +37,20 @@ import mint from './mint.png';
 import mistletoe from './mistletoe.png';
 import musu from './musu.png';
 import onyx from './onyx.png';
+import passport_1_bit_black_and_white_v2 from './passport_1_bit_black_and_white_v2.png';
+import passport_1_bit_green_v2 from './passport_1_bit_green_v2.png';
+import passport_1_bit_pink_v2 from './passport_1_bit_pink_v2.png';
+import passport_1_bit_white_and_red_v2 from './passport_1_bit_white_and_red_v2.png';
+import passport_blue from './passport_blue.png';
+import passport_green from './passport_green.png';
+import passport_grey from './passport_grey.png';
+import passport_lime_green from './passport_lime_green.png';
+import passport_orange from './passport_orange.png';
+import passport_pink from './passport_pink.png';
+import passport_purple from './passport_purple.png';
+import passport_red from './passport_red.png';
+import passport_white from './passport_white.png';
+import passportboxindex from './passportboxindex.png';
 import pine_cone from './pine_cone.png';
 import pine_pollen from './pine_pollen.png';
 import plastic_bottle from './plastic_bottle.png';
@@ -41,13 +64,16 @@ import resin from './resin.png';
 import respec_potion from './respec_potion.png';
 import ring_of_spirits from './ring_of_spirits.png';
 import ring_of_water_breathing from './ring_of_water_breathing.png';
+import river_of_life_card_native from './river_of_life_card_native.png';
 import scrap_metal from './scrap_metal.png';
 import screwdriver from './screwdriver.png';
 import scroll_of_shop_teleportation from './scroll_of_shop_teleportation.png';
 import shredded_mint from './shredded_mint.png';
 import spice_grinder from './spice_grinder.png';
 import stone from './stone.png';
+import strange_machine_part_native from './strange_machine_part_native.png';
 import sunset_apple_mochi from './sunset_apple_mochi.png';
+import VIPP from './VIPP.png';
 import wooden_stick from './wooden_stick.png';
 import xp_candy_huge from './xp_candy_huge.png';
 import xp_candy_large from './xp_candy_large.png';
@@ -56,6 +82,32 @@ import xp_candy_small from './xp_candy_small.png';
 import xp_potion from './xp_potion.png';
 
 export const ItemImages = {
+  river_of_life_card_native: river_of_life_card_native,
+  strange_machine_part_native: strange_machine_part_native,
+  VIPP: VIPP,
+  passport_white: passport_white,
+  passport_orange: passport_orange,
+  passport_pink: passport_pink,
+  passport_purple: passport_purple,
+  passport_red: passport_red,
+  passport_lime_green: passport_lime_green,
+  passportboxindex: passportboxindex,
+  passport_grey: passport_grey,
+  passport_green: passport_green,
+  passport_blue: passport_blue,
+  passport_1_bit_white_and_red_v2: passport_1_bit_white_and_red_v2,
+  passport_1_bit_pink_v2: passport_1_bit_pink_v2,
+  passport_1_bit_green_v2: passport_1_bit_green_v2,
+  passport_1_bit_black_and_white_v2: passport_1_bit_black_and_white_v2,
+  lotus_bud_card_native: lotus_bud_card_native,
+  lily_sprout_card_native: lily_sprout_card_native,
+  heroic_awakening_card_native: heroic_awakening_card_native,
+  flower_bud_card_native: flower_bud_card_native,
+  field_of_flowers_card_native: field_of_flowers_card_native,
+  eth: eth,
+  disc_shaped_map_native: disc_shaped_map_native,
+  astrolabe_part2: astrolabe_part2,
+  astrolabe_part3: astrolabe_part3,
   init: initia_icon,
   giftbox: agency_giftbox,
   astrolabe_disk: astrolabe_disk,
