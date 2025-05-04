80006b35b5362352c356c134cf2988fa81323cd5
hubur <hubur@asphodel.io>
2025-05-04 21:12:35 +0200

    🔇 Remove console logs

--- packages/client/src/workers/sync/snapshot/utils.ts
+++ packages/client/src/workers/sync/snapshot/utils.ts
@@ -9,17 +9,14 @@ async function checkFor403(url: string) {
     });
   } catch (e) {}
   is403 = response?.status == 403;
-  console.log('is403', is403);
   return is403;
 }
 
 function checkForGrpc8(e: any) {
   const errorCode = e.code || 'unknown';
-  console.log('errorCode', errorCode);
   return errorCode == 8;
 }
 
 export function isRateLimited(url: string, e: any) {
-  console.log('isRateLimited', e);
   return checkForGrpc8(e) || checkFor403(url);
 }
