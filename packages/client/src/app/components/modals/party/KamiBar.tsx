f06687ab82d750d4f84652f08a7d64b304b837bd
jirach <acheron@asphodel.io>
2025-05-04 04:05:22 -0400

    add tooltips

--- packages/client/src/app/components/modals/party/KamiBar.tsx
+++ packages/client/src/app/components/modals/party/KamiBar.tsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-import { calcHealth } from 'app/cache/kami';
-import { Text } from 'app/components/library';
+import { getHarvestItem } from 'app/cache/harvest';
+import { calcHealth, calcOutput, isDead, isHarvesting, isResting } from 'app/cache/kami';
+import { Text, Tooltip } from 'app/components/library';
 import { Cooldown } from 'app/components/library/cards/KamiCard/Cooldown';
 import { useSelected, useVisibility } from 'app/stores';
 import { AffinityIcons } from 'constants/affinities';
 import { Kami } from 'network/shapes/Kami';
+import { NullNode } from 'network/shapes/Node';
+import { getRateDisplay } from 'utils/numbers';
 import { playClick } from 'utils/sounds';
-
-/**
- * - amt
- * - rate?
- * - actions
- *
- */
+import { formatCountdown } from 'utils/time';
 
 interface Props {
   kami: Kami;
@@ -42,17 +39,27 @@ export const KamiBar = (props: Props) => {
     playClick();
   };
 
-  const getBodyIcon = () => {
+  const getBodyAffinity = () => {
     const body = kami.traits?.body;
-    if (!body || !body.affinity) return AffinityIcons.normal;
-    const affinityKey = body.affinity.toLowerCase() as keyof typeof AffinityIcons;
+    if (!body || !body.affinity) return 'NORMAL';
+    return body.affinity;
+  };
+
+  const getHandAffinity = () => {
+    const hand = kami.traits?.hand;
+    if (!hand || !hand.affinity) return 'NORMAL';
+    return hand.affinity;
+  };
+
+  const getBodyIcon = () => {
+    const affinity = getBodyAffinity();
+    const affinityKey = affinity.toLowerCase() as keyof typeof AffinityIcons;
     return AffinityIcons[affinityKey];
   };
 
   const getHandIcon = () => {
-    const hand = kami.traits?.hand;
-    if (!hand || !hand.affinity) return AffinityIcons.normal;
-    const affinityKey = hand.affinity.toLowerCase() as keyof typeof AffinityIcons;
+    const affinity = getHandAffinity();
+    const affinityKey = affinity.toLowerCase() as keyof typeof AffinityIcons;
     return AffinityIcons[affinityKey];
   };
 
@@ -61,14 +68,50 @@ export const KamiBar = (props: Props) => {
     return (100 * currentHealth) / total;
   };
 
+  const getTooltip = () => {
+    if (isDead(kami)) return ['Murdered', 'visit the waterfall or use a ribbon to revive'];
+
+    const totalHealth = kami.stats?.health.total ?? 0;
+    const healthRate = getRateDisplay(kami.stats!.health.rate, 2);
+
+    if (isResting(kami)) return [`${currentHealth}/${totalHealth}HP (${healthRate}/hr)`];
+
+    if (isHarvesting(kami) && kami.harvest) {
+      const tooltip: string[] = [];
+      const harvest = kami.harvest;
+      const spotRate = getRateDisplay(harvest.rates.total.spot, 2);
+      const avgRate = getRateDisplay(harvest.rates.total.average, 2);
+      const item = getHarvestItem(harvest);
+      const node = harvest.node ?? NullNode;
+
+      const duration = formatCountdown(tick / 1000 - harvest.time.last);
+      tooltip.push(`Averaging ${avgRate} ${item.name}/hr (${duration})`);
+      return [
+        `Harvesting on ${node.name}`,
+        `${calcOutput(kami)} ${item.name} (${spotRate} ${item.name}/hr)`,
+        `Averaging ${avgRate} ${item.name}/hr (${duration})`,
+      ];
+    }
+    return [];
+  };
+
   return (
-    <Container percent={calcHealthPercent()} color={getHealthColor(calcHealthPercent())}>
+    <Container>
       <Left>
         <Image src={kami.image} onClick={handleImageClick} />
-        <Icon src={getBodyIcon()} />
-        <Icon src={getHandIcon()} />
-        <Text size={0.9}>{kami.state}</Text>
+        <Tooltip
+          text={[`Body: ${getBodyAffinity()}`, `Hand: ${getHandAffinity()}`]}
+          direction='row'
+        >
+          <Icon src={getBodyIcon()} />
+          <Icon src={getHandIcon()} />
+        </Tooltip>
       </Left>
+      <Middle percent={calcHealthPercent()} color={getHealthColor(calcHealthPercent())}>
+        <Tooltip text={getTooltip()}>
+          <Text size={0.9}>{kami.state}</Text>
+        </Tooltip>
+      </Middle>
       <Actions>
         <Cooldown kami={kami} />
         {actions}
@@ -76,32 +119,28 @@ export const KamiBar = (props: Props) => {
     </Container>
   );
 };
-interface ContainerProps {
-  percent: number;
-  color: string;
-}
 
-const Container = styled.div<ContainerProps>`
+const Container = styled.div`
   border: 0.15vw solid black;
   border-radius: 0.6vw;
 
+  height: 100%;
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
   justify-content: space-between;
   overflow: hidden;
 
-  background: ${({ percent, color }) =>
-    `linear-gradient(90deg, ${color}, 0%, ${color}, ${percent * 0.95}%, #fff, ${Math.min(percent * 1.05, 100)}%, #fff 100%)`};
   user-select: none;
 `;
 
 const Left = styled.div`
+  gap: 0.3vw;
+
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
   justify-content: space-between;
-  gap: 0.6vw;
 `;
 
 const Actions = styled.div`
@@ -120,11 +159,34 @@ const Column = styled.div`
   justify-content: center;
 `;
 
+interface MiddleProps {
+  percent: number;
+  color: string;
+}
+const Middle = styled.div<MiddleProps>`
+  height: 3vw;
+  border-right: solid black 0.15vw;
+  border-left: solid black 0.15vw;
+  margin: 0 0.3vw 0 0.3vw;
+
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+
+  background: ${({ percent, color }) =>
+    `linear-gradient(90deg, ${color}, 0%, ${color}, ${percent}%, #fff, ${Math.min(percent * 1.05, 100)}%, #fff 100%)`};
+`;
+
 const Image = styled.img`
   width: 3vw;
   height: 3vw;
   cursor: pointer;
   border-right: solid black 0.15vw;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 const Icon = styled.img`
