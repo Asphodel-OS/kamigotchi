0d9905d67b80e633b3ab8d1e747e97de096f0829
Xenofluxx <susanainu@hotmail.com>
2025-05-07 18:07:09 +0200

    rebase

--- packages/client/src/assets/sound/ost/index.ts
+++ packages/client/src/assets/sound/ost/index.ts
@@ -4,7 +4,39 @@ import arrival from './arrival.mp3';
 import cave from './cave.mp3';
 
 import glitter from './glitter.mp3';
+import k1 from './k1.wav';
+import k11 from './k11.wav';
+import k13 from './k13.wav';
+import k14 from './k14.wav';
+import k2 from './k2.wav';
+import k3 from './k3.wav';
+import k4 from './k4.wav';
+import k5 from './k5.wav';
+import k8 from './k8.wav';
+import k9 from './k9.wav';
+import market from './market.wav';
+import mina from './mina.mp3';
 import mystique from './mystique.mp3';
 import shop from './shop.mp3';
 
-export { abandoned, amusement, arrival, cave, glitter, mystique, shop };
+export {
+  abandoned,
+  amusement,
+  arrival,
+  cave,
+  glitter,
+  k1,
+  k11,
+  k13,
+  k14,
+  k2,
+  k3,
+  k4,
+  k5,
+  k8,
+  k9,
+  market,
+  mina,
+  mystique,
+  shop,
+};
