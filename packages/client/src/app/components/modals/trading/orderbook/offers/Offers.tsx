5b816e7fa0da25cf6413483de047fb442361df87
jirach <acheron@asphodel.io>
2025-07-02 03:39:48 -0400

    delete some old stuff

--- packages/client/src/app/components/modals/trading/orderbook/offers/Offers.tsx
+++ packages/client/src/app/components/modals/trading/orderbook/offers/Offers.tsx
@@ -82,11 +82,6 @@ export const Offers = (props: Props) => {
     <Container>
       <Title>Open Offers</Title>
       <Body>
-        {/* {displayed.map((trade, i) => {
-          const type = getTradeType(trade, false);
-          if (type === 'Sell') return <SellOrder key={i} actions={actions} data={{ trade }} />;
-          return <BuyOrder key={i} actions={actions} data={{ trade }} />;
-        })} */}
         {displayed.map((trade, i) => {
           const type = getTradeType(trade, false);
           return (
