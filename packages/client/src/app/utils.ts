72ec708d8873c764be9e5506a16229f2a3109b50
Xenofluxx <susanainu@hotmail.com>
2025-08-28 19:30:04 +0200

    check mousebutton clicked

--- packages/client/src/app/utils.ts
+++ packages/client/src/app/utils.ts
@@ -5,3 +5,21 @@ export const copy = (str: string) => {
   playClick();
   navigator.clipboard.writeText(str);
 };
+
+// returns which mouse button was clicked
+export const mouseBttnClicked = (e: MouseEvent): string => {
+  switch (e.button) {
+    case 0:
+      return 'left';
+    case 1:
+      return 'middle';
+    case 2:
+      return 'right';
+    case 3:
+      return 'back';
+    case 4:
+      return 'forward';
+    default:
+      return 'unknown';
+  }
+};
