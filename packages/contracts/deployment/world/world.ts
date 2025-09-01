a3b4d5634eb3c09cb8af083ebc3aa3f806d74ffc
jirach <acheron@asphodel.io>
2025-09-01 03:58:02 -0400

    fix up token deployment scripts

--- packages/contracts/deployment/world/world.ts
+++ packages/contracts/deployment/world/world.ts
@@ -13,7 +13,7 @@ import {
   deleteRelationships,
   deleteRooms,
   deleteSkills,
-  deleteToken,
+  deleteTokens,
   initAll,
   initAllLocal,
   initAllTesting,
@@ -30,12 +30,12 @@ import {
   initMintConfigs,
   initNodes,
   initNpcs,
-  initPortal,
   initQuests,
   initRecipes,
   initRelationships,
   initRooms,
   initSkills,
+  initTokens,
   initTradeConfigs,
   initTraits,
   mintToGachaPool,
@@ -50,6 +50,9 @@ import {
   reviseRooms,
   reviseSkills,
 } from './state';
+
+// TODO: rename this file to something that makes more sense
+// ('api.ts' would conflict with api/)
 
 export type WorldAPI = typeof WorldState.prototype.api;
 
@@ -89,10 +92,6 @@ export class WorldState {
     },
     auth: {
       init: () => this.genCalls(initAuth),
-    },
-    bridge: {
-      init: () => this.genCalls(initPortal),
-      delete: (index: number) => this.genCalls((api) => deleteToken(api, index)),
     },
     config: {
       init: () => this.genCalls(initConfigs),
@@ -134,6 +133,10 @@ export class WorldState {
     } as SubFunc,
     mint: {
       init: () => this.genCalls((api) => initGachaPool(api, 333)),
+    } as SubFunc,
+    portal: {
+      init: (indices: number[]) => this.genCalls((api) => initTokens(api, indices)),
+      delete: (indices: number[]) => this.genCalls((api) => deleteTokens(api, indices)),
     } as SubFunc,
     quests: {
       init: (indices?: number[]) => this.genCalls((api) => initQuests(api, indices)),
