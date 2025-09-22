ed7d701728ede185d3dc4e8af08439b62a0c03b4
Xenofluxx <susanainu@hotmail.com>
2025-09-22 22:08:18 +0200

    change pdf to plain text

--- packages/client/src/app/components/modals/help/Page.tsx
+++ packages/client/src/app/components/modals/help/Page.tsx
@@ -1,29 +1,18 @@
 import styled from 'styled-components';
 
-import { TermsAndConditions } from 'assets/documents';
 import { HelpTabs } from './types';
 
 export const Page = ({ body, tab }: { body: string[]; tab: HelpTabs }) => {
   return (
-    <Container>
+    <Container lang='en'>
       {body.map((line: string, i: number) => {
         return (
-          <Line key={i}>
+          <Line lang='en' key={i}>
             {line}
             <br />
           </Line>
         );
       })}
-      {tab === HelpTabs.TERMS && (
-        <GasLink
-          key='terms&conditions'
-          href={TermsAndConditions}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Our Terms & Conditions
-        </GasLink>
-      )}
     </Container>
   );
 };
@@ -32,19 +21,15 @@ const Container = styled.div`
   color: #333;
   padding: 1.5vw;
   position: relative;
+  white-space: pre-wrap;
 `;
 
 const Line = styled.div`
   font-size: 0.9vw;
   line-height: 150%;
-  text-align: left;
-`;
-
-const GasLink = styled.a<{ linkColor?: string }>`
-  color: ${({ linkColor }) => linkColor ?? '#145006ff'};
-  font-size: 0.8vw;
-  text-decoration: underline;
-  &:hover {
-    text-decoration: none;
-  }
+  text-align: justify;
+  white-space: pre-wrap;
+  word-break: normal;
+  overflow-wrap: break-word;
+  hyphens: auto;
 `;
