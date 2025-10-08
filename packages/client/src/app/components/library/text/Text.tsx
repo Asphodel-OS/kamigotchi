cae71660c2edfc29e14c0e31a52df96749127be4
Xenofluxx <susanainu@hotmail.com>
2025-10-08 17:05:53 +0200

    code rabbit

--- packages/client/src/app/components/library/text/Text.tsx
+++ packages/client/src/app/components/library/text/Text.tsx
@@ -15,7 +15,8 @@ export const Text = styled.div<{
   line-height: ${({ size }) => size * 1.5}vw;
   color: ${({ color }) => color ?? '#333'};
   ${({ limit }) =>
-    limit && `max-width:${limit}ch;     overflow: hidden;  text-overflow: ellipsis;`};
+    limit &&
+    `max-width: ${limit}ch; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;`};
 
   padding: ${({ padding }) => padding?.top ?? 0}vw ${({ padding }) => padding?.right ?? 0}vw
     ${({ padding }) => padding?.bottom ?? 0}vw ${({ padding }) => padding?.left ?? 0}vw;
