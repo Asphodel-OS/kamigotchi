0fca097ec4bb3dec661369f5099a6e4906d83f31
Xenofluxx <susanainu@hotmail.com>
2025-11-13 12:32:35 +0100

    add indexes

---
+++ packages/client/src/assets/images/rooms/59_black-pool/index.ts
@@ -0,0 +1,5 @@
+import bgPlaytestDay from './backgrounds/room59a.png';
+import bgPlaytestEvening from './backgrounds/room59b.png';
+import bgPlaytestNight from './backgrounds/room59c.png';
+
+export { bgPlaytestDay, bgPlaytestEvening, bgPlaytestNight };
