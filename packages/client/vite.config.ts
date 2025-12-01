eaf7f87ad16701ab16a165f3d69c7b4a61da9ce9
leche <lechedeburro420@gmail.com>
2025-12-01 17:35:40 -0500

    remove target react version, 19 is default and matches app

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,17 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-const ReactCompilerConfig = {
-  target: '19',
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     dsv(),
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
   ],
