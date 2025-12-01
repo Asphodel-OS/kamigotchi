b577da0d2490fb9c087f985e06b3c49ac69d1448
leche <lechedeburro420@gmail.com>
2025-12-01 12:01:26 -0500

    enable react 19 complier

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const ReactCompilerConfig = {
+  target: '19',
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dsv(), react()],
+  plugins: [
+    dsv(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
   assetsInclude: ['**/*.gif', '**/*.jpg', '**/*.mp3', '**/*.png', '**/*.wav', '**/*.webp'],
   build: {
     assetsInlineLimit: 0,
