bcf4d8b83cb608ba1a964a156e0d93dfe984f157
Xenofluxx <susanainu@hotmail.com>
2025-12-19 15:11:08 +0100

    change backgrounds

--- packages/client/src/assets/images/rooms/2_tree-tunnel/index.ts
+++ packages/client/src/assets/images/rooms/2_tree-tunnel/index.ts
@@ -1,10 +1,7 @@
-import bgOriginal from './backgrounds/original.png';
-// import bgPlaytestDay from './backgrounds/playtest-a.png';
-// import bgPlaytestEvening from './backgrounds/playtest-b.png';
-// import bgPlaytestNight from './backgrounds/playtest-c.png';
 import bgPlaytestDay from './backgrounds/glow-a.png';
 import bgPlaytestEvening from './backgrounds/glow-b.png';
 import bgPlaytestNight from './backgrounds/glow-c.png';
+import bgOriginal from './backgrounds/original.png';
 import bgPretest from './backgrounds/pretest.png';
 import bgChristmasDay from './backgrounds/room2a1-christmas.png';
 import bgChristmasEvening from './backgrounds/room2b1-christmas.png';
@@ -16,14 +13,14 @@ import objectHollowStump from './objects/hollow-stump.png';
 import objectShopDoor from './objects/shop-door.png';
 
 export {
+  bgChristmasDay,
+  bgChristmasEvening,
+  bgChristmasNight,
   bgOriginal,
   bgPlaytestDay,
   bgPlaytestEvening,
   bgPlaytestNight,
   bgPretest,
-  bgChristmasDay,
-  bgChristmasEvening,
-  bgChristmasNight,
   objectClearing,
   objectGate,
   objectHollowStump,
