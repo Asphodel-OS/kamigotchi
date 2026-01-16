c2b3e048f67c610601e7c542570469571ab074ae
Xenofluxx <susanainu@hotmail.com>
2026-01-16 20:35:18 +0100

    add dot

--- packages/client/src/constants/dialogue/70_still-stream.ts
+++ packages/client/src/constants/dialogue/70_still-stream.ts
@@ -2,7 +2,7 @@ import { DialogueNode } from '.';
 
 const centralstalagmite: DialogueNode = {
   index: 701,
-  text: ['Something is carved here.', '"Y&J4VR"'],
+  text: ['Something is carved here.', '"Y&J4VR".'],
 };
 
 export default [centralstalagmite];
