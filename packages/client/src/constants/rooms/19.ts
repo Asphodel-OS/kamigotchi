ed0f0ff8451c613eed9b87c3e9caa78058445291
Thorne 🌸 <ExistentialEnso@users.noreply.github.com>
2026-02-10 13:44:38 -0500

    Setup Secrets of the Ooze co-op goal (#2352)
    This PR just includes data changes (including both in CSVs and stuff
    still hardcoded) to support the new co-op quest.
    
    * New co-op goal added
    * Clicking on the ooze in the temple now brings up co-op goal modal
    instead of dialog
    * The two ooze rooms are now linked, though we will need to change what
    clicking the ooze brings up post co-op still
    * New coat check ticket item added (reward for the co-op)
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    * **New Features**
    * Added "Secret of the Ooze" goal with rewards including items and
    unlocks fast travel between locations.
    * Updated interaction model for the black pool to trigger the new goal
    upon player interaction.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/client/src/constants/rooms/19.ts
+++ packages/client/src/constants/rooms/19.ts
@@ -1,3 +1,4 @@
+import { triggerGoalModal } from 'app/triggers';
 import { bgPlaytestDay } from 'assets/images/rooms/19_temple-of-the-wheel';
 import { templeOfTheWheel } from 'assets/sound/ost';
 import { Room } from './types';
@@ -13,7 +14,7 @@ export const room19: Room = {
     {
       name: 'black pool',
       coordinates: { x1: 35, y1: 95, x2: 95, y2: 107 },
-      dialogue: 192,
+      onClick: () => triggerGoalModal([13]),
     },
     {
       name: 'Dimidiatus',
