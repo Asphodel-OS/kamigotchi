4b26093729f47b4829b862de2659033d35d2cee9
Xenofluxx <susanainu@hotmail.com>
2026-02-13 14:58:20 +0100

    proper external kami

--- packages/client/src/app/components/modals/marketplace/MarketPlace.tsx
+++ packages/client/src/app/components/modals/marketplace/MarketPlace.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import { useReadContracts, useWatchBlockNumber } from 'wagmi';
 
 import { getAccountKamis as _getAccountKamis } from 'app/cache/account';
+import { getConfigAddress } from 'app/cache/config';
+import { getKami as _getKami } from 'app/cache/kami';
 import { ModalHeader, ModalWrapper } from 'app/components/library';
 import { useLayers } from 'app/root/hooks';
 import { UIComponent } from 'app/root/types';
-import { useNetwork } from 'app/stores';
+import { useAccount, useNetwork } from 'app/stores';
 import VendIcon from 'assets/images/rooms/18_cave-crossroads/vend.png';
+import { EntityIndex } from 'engine/recs';
 import { BigNumberish } from 'ethers';
+import { erc721ABI } from 'network/chain/ERC721';
 import { queryAccountFromEmbedded } from 'network/shapes/Account';
-import { getAllKamis as _getAllKamis } from 'network/shapes/Kami';
+import {
+  getAllKamis as _getAllKamis,
+  queryKamiByIndex as _queryKamiByIndex,
+} from 'network/shapes/Kami';
 import { getRegistryTraits as _getRegistryTraits, TraitType } from 'network/shapes/Trait';
 import { Bids } from './Bids';
 import { CreateOrder } from './CreateOrder';
@@ -22,24 +30,54 @@ export const MarketPlaceModal: UIComponent = {
   Render: () => {
     const {
       utils,
+      data,
       network: { actions, api },
     } = (() => {
       const { network } = useLayers();
       const { world, components } = network;
       const accountEntity = queryAccountFromEmbedded(network);
 
+      const kamiNFTAddress = getConfigAddress(world, components, 'KAMI721_ADDRESS');
+
       return {
         utils: {
-          getAccountKamis: () => _getAccountKamis(world, components, accountEntity),
+          getAccountKamis: () => _getAccountKamis(world, components, accountEntity, { live: 0 }),
           getAllKamis: () => _getAllKamis(world, components),
           getRegistryTraits: (specificType?: TraitType[]) =>
             _getRegistryTraits(world, components, specificType),
+          getKami: (entity: EntityIndex) => _getKami(world, components, entity, { live: 0 }),
+          queryKamiByIndex: (index: number) => _queryKamiByIndex(world, components, index),
         },
         network,
+        data: { kamiNFTAddress },
       };
     })();
     const apis = useNetwork((s) => s.apis);
     const selectedAddress = useNetwork((s) => s.selectedAddress);
+    const account = useAccount((s) => s.account);
+
+    const { refetch: refetchNFTs, data: nftData } = useReadContracts({
+      contracts: [
+        {
+          address: data.kamiNFTAddress,
+          abi: erc721ABI,
+          functionName: 'getAllTokens',
+          args: [account.ownerAddress],
+        },
+      ],
+    });
+
+    useWatchBlockNumber({
+      onBlockNumber: () => refetchNFTs(),
+    });
+
+    const externalKamis = useMemo(() => {
+      const result = (nftData?.[0]?.result ?? []) as number[];
+      const entities = result
+        .map((index: number) => utils.queryKamiByIndex(index))
+        .filter((entity) => !!entity) as EntityIndex[];
+      return entities.map((entity) => utils.getKami(entity));
+    }, [nftData]);
 
     const createSellOrder = (kamiIndex: number, price: BigNumberish, expiry: BigNumberish) => {
       const api = apis.get(selectedAddress);
@@ -105,13 +143,13 @@ export const MarketPlaceModal: UIComponent = {
           showCreateOrder={showCreateOrder}
           setShowFilter={setShowFilter}
           onCloseCreateOrder={closeCreateOrder}
-          utils={utils}
+          utils={{ ...utils, getExternalKamis: () => externalKamis }}
         />
         <MyOrders isVisible={tab === 'myOrders'} />
         <CreateOrder
           isVisible={showCreateOrder}
           onClose={closeCreateOrder}
-          utils={utils}
+          utils={{ ...utils, getExternalKamis: () => externalKamis }}
           createSellOrder={createSellOrder}
           createBuyOrder={createBuyOrder}
           createBuyKamiOrder={createBuyKamiOrder}
