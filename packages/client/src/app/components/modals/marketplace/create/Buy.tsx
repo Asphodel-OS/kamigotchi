f1064daa7c51c3ab97c2f91d18198fbde39f6d6d
Xenofluxx <susanainu@hotmail.com>
2026-02-18 17:54:04 +0100

    wei

--- packages/client/src/app/components/modals/marketplace/create/Buy.tsx
+++ packages/client/src/app/components/modals/marketplace/create/Buy.tsx
@@ -74,8 +74,7 @@ export const Buy = ({
               value={price}
               onChange={(e) => {
                 const val = e.target.value;
-                // TODO: remove \.? if we dont want decimals
-                if (val === '' || /^\d*\.?\d*$/.test(val)) setPrice(val);
+                if (val === '' || /^\d*\.?\d{0,18}$/.test(val)) setPrice(val);
               }}
             />
             <EthIcon src={TokenIcons.eth} alt='ETH' />
