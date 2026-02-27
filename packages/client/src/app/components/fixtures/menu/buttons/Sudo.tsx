33e50e07d048c35b58d65eea2a38bdd7007ac6a0
kanyaveral <jalvfer@outlook.com>
2026-02-27 19:05:26 +0100

    Remove Sudoswap from external apps dropdown menu

--- packages/client/src/app/components/fixtures/menu/buttons/Sudo.tsx
+++ packages/client/src/app/components/fixtures/menu/buttons/Sudo.tsx
@@ -1,12 +1,7 @@
 import { IconListButton } from 'app/components/library';
-import { ExternalIcon, SudoIcon, Whispo } from 'assets/images/icons/menu';
-
-const KAMI_ADDR = '0x5d4376b62fa8ac16dfabe6a9861e11c33a48c677';
+import { ExternalIcon, Whispo } from 'assets/images/icons/menu';
 
 export const SudoMenuButton = () => {
-  const openSudoLink = () => {
-    window.open(`https://sudoswap.xyz/#/browse/yominet/buy/${KAMI_ADDR}`, '_blank', 'noopener');
-  };
   const openKamibotsLink = () => {
     window.open(`https://www.kamibots.xyz`, '_blank', 'noopener');
   };
@@ -15,7 +10,6 @@ export const SudoMenuButton = () => {
     <IconListButton
       img={ExternalIcon}
       options={[
-        { text: 'Sudoswap', image: SudoIcon, onClick: openSudoLink },
         { text: 'KamiBots', image: Whispo, onClick: openKamibotsLink },
       ]}
       scale={4.5}
