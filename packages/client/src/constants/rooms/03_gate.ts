a6598c4c7d2e755623829aebbe534ab84fa7fd5c
Xenofluxx <susanainu@hotmail.com>
2026-03-09 10:29:46 +0100

    add zevanas code for fetching alternative dialogue

--- packages/client/src/constants/rooms/03_gate.ts
+++ packages/client/src/constants/rooms/03_gate.ts
@@ -1,6 +1,27 @@
+import { triggerDialogueModal } from 'app/triggers/triggerDialogueModal';
 import { bgPlaytestDay, bgPlaytestEvening, bgPlaytestNight } from 'assets/images/rooms/3_gate';
 import { arrival } from 'assets/sound/ost';
+import {
+  getAccountFromEmbedded,
+  queryAccountFromEmbedded,
+  queryAccountKamis,
+} from 'network/shapes/Account';
 import { Room } from './types';
+
+const NEWBIE_VENDOR_MAX_ACCOUNT_AGE_SECONDS = 24 * 60 * 60;
+
+const onClickZevanaDialogue = () => {
+  const network = (window as any).network;
+  const { world, components } = network;
+  const accountEntity = queryAccountFromEmbedded(network);
+  const account = getAccountFromEmbedded(network);
+  const hasAnyKamis = queryAccountKamis(world, components, accountEntity).length > 0;
+  const now = Math.floor(Date.now() / 1000);
+  const isNewbie = now - account.time.creation <= NEWBIE_VENDOR_MAX_ACCOUNT_AGE_SECONDS;
+  //const dialogueIndex = !hasAnyKamis && isNewbie ? alternativeindex : 30001;
+  const dialogueIndex = 30001;
+  triggerDialogueModal(dialogueIndex);
+};
 
 export const room03: Room = {
   index: 3,
@@ -15,6 +36,10 @@ export const room03: Room = {
     //   coordinates: { x1: 48, y1: 29, x2: 123, y2: 85 },
     //   onClick: () => triggerGoalModal([6]),
     // },
-    { name: 'Zevana', coordinates: { x1: 10, y1: 30, x2: 80, y2: 132 }, dialogue: 30001 },
+    {
+      name: 'Zevana',
+      coordinates: { x1: 10, y1: 30, x2: 80, y2: 132 },
+      onClick: onClickZevanaDialogue,
+    },
   ],
 };
